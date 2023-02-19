492b332ad357c8ae63d8eee27749c3cf1f4b2c84
Ihor Leonov <ihorleonow@gmail.com>
2023-02-19 22:37:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { theme } from 'components/constants/theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <App></App>
     </ThemeProvider>
   </React.StrictMode>
 );
